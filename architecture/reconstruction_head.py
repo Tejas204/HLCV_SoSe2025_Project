f46fc13a0795ccd1c13e09c394b4805679b8ba3f
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-07-03 08:08:37 +0200

    Fused weights code

---
+++ architecture/reconstruction_head.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class RECONSTRUCTION_HEAD(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(RECONSTRUCTION_HEAD, self).__init__()
+
+        self.decoder = nn.Sequential(
+            nn.Conv2d(in_channels, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, out_channels, kernel_size=3, padding=1),
+            nn.Tanh()  # Assuming image values are normalized between [-1, 1]
+        )
+
+    def forward(self, x):
+        return self.decoder(x)
